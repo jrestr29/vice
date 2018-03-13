b5fbe2d53f8d8dc725681df91c7ebcde437ed073
Jose D <josda6486@gmail.com>
2018-03-12 22:48:03 -0500

    Progress made on web GUI

--- js/vice.js
+++ js/vice.js
@@ -3,5 +3,7 @@
 })
 
 $(document).ready(function(){
+	$('nav').addClass('load');
+	//$('nav').css('opacity', '1');
 	//$('header').css('min-height', $(window).height()); //Make initial shot fit browser viewport height
 });
